da5f34426a266b6d8bc9f10df3d9282bb209a7f5
anna-liang <annaliang56@gmail.com>
2021-07-12 00:48:30 -0400

    Homepage background

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,1 +1,2 @@
 // https://medium.com/how-to-react/scroll-to-an-element-on-click-in-react-js-8424e478bb9
+// https://www.pexels.com/video/scrolling-codes-on-computer-5473795/
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import Typical from 'react-typical'
+import '../styles/Home.css'
+import backgroundVideo from '../media/background-video.mp4'
 
 class Home extends Component {
     render () {
         return (
-            <div id="home">
-                <div id="home" style={{height: 500}}>
+            <div className="background-video-container">
+                <div className="background-video-overlay"></div>
+                <div className="background-video">
+                    <video
+                        playsInline="playsinline"
+                        autoPlay="autoplay"
+                        muted="muted"
+                        loop="loop"
+                    >
+                        <source src={backgroundVideo} type="video/mp4"/>
+                    </video>
+                </div>
+                <div className="background-video-content" id="home">
                     <Typical
                         steps={['Anna Liang']}
                         wrapper='h1'
+                        className="title"
                     />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.</p>
+                    <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.</p>
                 </div>
             </div>
         )
