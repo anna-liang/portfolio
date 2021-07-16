1055c676fe3a4ae8ec41a9109fde166fa767967a
anna-liang <annaliang56@gmail.com>
2021-07-16 00:29:25 -0400

    Changed header colours

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,33 @@ import { Link } from 'react-scroll'
 import '../styles/Header.css'
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggleClass = this.toggleClass.bind(this);
+        this.state = {
+            active: false,
+        };
+    }
+
+    toggleClass() {
+        console.log("toggle");
+        const currentState = this.state.active;
+        this.setState({
+            active: !currentState,
+        });
+    }
+
     render () {
         return (
-            <ul id="header">
-                <li><Link activeClass="active" to="home" smooth={true}>Home</Link></li>
-                <li><Link to="about" smooth={true}>About</Link></li>
-                <li><Link to="projects" smooth={true}>Projects</Link></li>
-                <li><Link to="contact" smooth={true}>Contact</Link></li>
-            </ul>
+            <div className="header-container">
+                <ul id="header">
+                    <li className={this.state.active ? "selected" : null}><Link activeClass="active" to="home" smooth={true} onClick={() => this.toggleClass()}>Home</Link></li>
+                    <li className={this.state.active ? "selected" : null} onClick={() => this.toggleClass()}><Link to="about" smooth={true}>About</Link></li>
+                    <li className={this.state.active ? "selected" : null} onClick={() => this.toggleClass()}><Link to="projects" smooth={true}>Projects</Link></li>
+                    <li className={this.state.active ? "selected" : null} onClick={() => this.toggleClass()}><Link to="contact" smooth={true}>Contact</Link></li>
+                </ul>
+            </div>
         )
     }
 }
