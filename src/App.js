96d67a92438924b0fc69d2fa0430fc8b79566973
anna-liang <annaliang56@gmail.com>
2021-07-18 23:15:21 -0400

    Header selection colour

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import About from './components/About';
 import Section from './components/Section';
 
-function App() {
-  return (
-    <Fragment>
-      <Header/>
-      <Home/>
-      <About/>
-      <Projects/>
-      <Section/>
-    </Fragment>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.toggleClass = this.toggleClass.bind(this);
+    this.scrollHandler = this.scrollHandler.bind(this);
+    this.state = {
+      active: Array(1).fill(true).concat(Array(3).fill(false)),
+      scrollOffset: window.pageYOffset,
+    }
+  }
+
+  toggleClass(i) {
+    const currentActive = this.state.active;
+    for (let j = 0; j < 4; j++)
+    {
+        if (j === i || currentActive[j])
+          currentActive[j] = !currentActive[j];
+    }
+    this.setState({
+        active: currentActive,
+    });
+  }
+
+  scrollHandler() {
+    this.setState({
+      scrollOffset: window.pageYOffset,
+    });
+    let currentActive = Array(4).fill(false);
+    if (this.state.scrollOffset < 700)
+      currentActive[0] = true;
+    else if (this.state.scrollOffset > 700 && this.state.scrollOffset < 1400)
+      currentActive[1] = true;
+    else if (this.state.scrollOffset >= 1400 && this.state.scrollOffset < 2000)
+      currentActive[2] = true;
+    else if (this.state.scrollOffset >= 2000)
+      currentActive[3] = true;
+    this.setState({
+      active: currentActive,
+    });
+  }
+
+  componentDidMount = () => window.addEventListener('scroll', this.scrollHandler);
+
+  componentWillUnmount = () => window.removeEventListener('scroll', this.scrollHandler);
+
+  render() {
+    return (
+      <Fragment>
+        <Header 
+          active={this.state.active} 
+          onClick={(i) => this.toggleClass(i)} 
+        />
+        <Home/>
+        <About/>
+        <Projects/>
+        <Section/>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,14 @@ import '../styles/Header.css'
 
 class Header extends Component {
 
-    constructor(props) {
-        super(props);
-        this.toggleClass = this.toggleClass.bind(this);
-        this.state = {
-            active: false,
-        };
-    }
-
-    toggleClass() {
-        console.log("toggle");
-        const currentState = this.state.active;
-        this.setState({
-            active: !currentState,
-        });
-    }
-
     render () {
         return (
             <div className="header-container">
                 <ul id="header">
-                    <li className={this.state.active ? "selected" : null}><Link activeClass="active" to="home" smooth={true} onClick={() => this.toggleClass()}>Home</Link></li>
-                    <li className={this.state.active ? "selected" : null} onClick={() => this.toggleClass()}><Link to="about" smooth={true}>About</Link></li>
-                    <li className={this.state.active ? "selected" : null} onClick={() => this.toggleClass()}><Link to="projects" smooth={true}>Projects</Link></li>
-                    <li className={this.state.active ? "selected" : null} onClick={() => this.toggleClass()}><Link to="contact" smooth={true}>Contact</Link></li>
+                    <li className={this.props.active[0] ? "selected" : null}><Link activeClass="active" to="home" smooth={true} onClick={() => this.props.onClick(0)}>Home</Link></li>
+                    <li className={this.props.active[1] ? "selected" : null}><Link to="about" smooth={true} onClick={() => this.props.onClick(1)}>About</Link></li>
+                    <li className={this.props.active[2] ? "selected" : null}><Link to="projects" smooth={true} onClick={() => this.props.onClick(2)}>Projects</Link></li>
+                    <li className={this.props.active[3] ? "selected" : null}><Link to="contact" smooth={true} onClick={() => this.props.onClick(3)}>Contact</Link></li>
                 </ul>
             </div>
         )
