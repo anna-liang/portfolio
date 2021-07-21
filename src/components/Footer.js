40f8a6f4d76be759d9974845041e59c0877adb02
anna-liang <annaliang56@gmail.com>
2021-07-21 00:31:14 -0400

    Added credits button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Credits from './Credits';
 import '../styles/Footer.css';
 
 function Footer(props) {
 
     return (
-        <footer>
-            <p>Created and designed by <span className="name">Anna Liang</span> &copy; 2021</p>
-        </footer>
+        <Router>
+            <footer>
+                <div className="credits-btn">
+                    <Link to="/portfolio/credits">
+                        <input type="submit" className="btn btn-outline-light" value="Credits"/>
+                    </Link>
+                </div>
+                <p>Created and designed by <span className="name">Anna Liang</span> &copy; 2021</p>
+            </footer>
+            <Switch>
+                <Route exact path="/credits" component={Credits}></Route>
+            </Switch>
+        </Router>
     );
 }
 
