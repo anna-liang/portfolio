6d99824870cf8a53a9fa77a794efba66fe68082d
anna-liang <annaliang56@gmail.com>
2021-07-21 00:42:43 -0400

    Changed background video

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,3 +1,16 @@
 // https://medium.com/how-to-react/scroll-to-an-element-on-click-in-react-js-8424e478bb9
 // https://www.pexels.com/video/scrolling-codes-on-computer-5473795/
 // https://www.youtube.com/watch?v=NgWGllOjkbs
+// https://pixabay.com/videos/black-black-friday-background-loop-13495/
+
+import React from 'react';
+
+function Credits(props) {
+    return (
+        <h1>
+            testing
+        </h1>
+    );
+}
+
+export default Credits;
