e6bf750d1f1806e985efa50d225cd39931848a79
anna-liang <annaliang56@gmail.com>
2021-07-22 21:18:07 -0400

    Updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     render () {
         return (
             <div className="header-container">
-                <ul id="header">
+                <ul className="header">
                     <li className={this.props.active[0] ? "selected" : null}><Link activeClass="active" to="home" smooth={true} onClick={() => this.props.onClick(0)}>Home</Link></li>
                     <li className={this.props.active[1] ? "selected" : null}><Link to="about" smooth={true} onClick={() => this.props.onClick(1)}>About</Link></li>
                     <li className={this.props.active[2] ? "selected" : null}><Link to="projects" smooth={true} onClick={() => this.props.onClick(2)}>Projects</Link></li>
