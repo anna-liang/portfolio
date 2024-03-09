0260e2da6cbdd9efd3b60c3f627404a0665d5d9a
anna <annaliang56@gmail.com>
2024-03-09 15:46:08 -0500

    Add new header section for work

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from './components/Header';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Credits from './components/Credits';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { Projects } from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Credits from "./components/Credits";
+import { WorkExperience } from "./components/WorkExperience";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.toggleClass = this.toggleClass.bind(this);
     this.scrollHandler = this.scrollHandler.bind(this);
     this.state = {
-      active: Array(1).fill(true).concat(Array(3).fill(false)),
+      active: Array(1).fill(true).concat(Array(4).fill(false)),
       scrollOffset: window.scrollY,
-    }
+    };
   }
 
   toggleClass(i) {
     const currentActive = this.state.active;
-    for (let j = 0; j < 4; j++)
-    {
-        if (j === i || currentActive[j])
-          currentActive[j] = !currentActive[j];
+    for (let j = 0; j < 5; j++) {
+      if (j === i || currentActive[j]) currentActive[j] = !currentActive[j];
     }
     this.setState({
-        active: currentActive,
+      active: currentActive,
     });
   }
 
@@ -37,41 +35,49 @@ class App extends Component {
     this.setState({
       scrollOffset: window.scrollY,
     });
-    let currentActive = Array(4).fill(false);
-    if (this.state.scrollOffset < 700)
-      currentActive[0] = true;
-    else if (this.state.scrollOffset > 700 && this.state.scrollOffset < 1400)
+    let currentActive = Array(5).fill(false);
+    // home
+    if (this.state.scrollOffset < 700) currentActive[0] = true;
+    // about
+    else if (this.state.scrollOffset > 700 && this.state.scrollOffset < 1800)
       currentActive[1] = true;
-    else if (this.state.scrollOffset >= 1400 && this.state.scrollOffset < 2400)
+    // work
+    else if (this.state.scrollOffset >= 1800 && this.state.scrollOffset < 2600)
       currentActive[2] = true;
-    else if (this.state.scrollOffset >= 2400)
+    // projects
+    else if (this.state.scrollOffset >= 2600 && this.state.scrollOffset < 4000)
       currentActive[3] = true;
+    // contact
+    else if (this.state.scrollOffset >= 4000) currentActive[4] = true;
     this.setState({
       active: currentActive,
     });
   }
 
-  componentDidMount = () => window.addEventListener('scroll', this.scrollHandler);
+  componentDidMount = () =>
+    window.addEventListener("scroll", this.scrollHandler);
 
-  componentWillUnmount = () => window.removeEventListener('scroll', this.scrollHandler);
+  componentWillUnmount = () =>
+    window.removeEventListener("scroll", this.scrollHandler);
 
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/portfolio">
-            <Header 
-              active={this.state.active} 
-              onClick={(i) => this.toggleClass(i)} 
+            <Header
+              active={this.state.active}
+              onClick={(i) => this.toggleClass(i)}
             />
-            <Home/>
-            <About/>
-            <Projects/>
-            <Contact/>
-            <Footer/>
+            <Home />
+            <About />
+            <WorkExperience />
+            <Projects />
+            <Contact />
+            <Footer />
           </Route>
           <Route path="/portfolio/credits">
-            <Credits/>
+            <Credits />
           </Route>
         </Switch>
       </Router>
