aa78477b7ec7044f58086f11efe09ad4d563b55e
anna <annaliang56@gmail.com>
2024-03-11 16:01:04 -0400

    Clean up styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ export const Contact = () => {
     <div className="contact-container contact">
       <h1 className="subheading">Contact</h1>
       <p className="contact-desc">
-        If you have any questions, feel free to email me at{" "}
-        <span className="name">annaliang.contact@gmail.com</span>.
+        If you have any questions, feel free to email me at
+        <span className="name"> annaliang.contact@gmail.com</span>.
       </p>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Typewriter from "typewriter-effect";
 import "../styles/Home.css";
 import backgroundVideo from "../media/background-video.mp4";
