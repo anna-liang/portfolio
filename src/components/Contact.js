bb64f5d0c53393ad5e2deab22247cb2fe4963de2
anna <annaliang56@gmail.com>
2025-09-02 12:44:53 -0400

    Reword contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "../styles/Contact.css";
-import "../styles/Main.css";
+import React from 'react';
+import '../styles/Contact.css';
+import '../styles/Main.css';
 
 export const Contact = () => {
   return (
     <div className="contact-container contact">
       <h1 className="subheading">Contact</h1>
       <p className="contact-desc">
-        If you have any questions, feel free to email me at
+        If you would like to reach out, feel free to email me at
         <span className="name"> annaliang.contact@gmail.com</span>.
       </p>
     </div>
